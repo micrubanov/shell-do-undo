978dd30b9ded503b117084223386cf59942603a8
Michael Rubanov <michael.r@anzu.io>
2018-07-11 00:50:51 +0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to shell-do-undo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/shell_do_undo/core_test.clj
@@ -0,0 +1,7 @@
+(ns shell-do-undo.core-test
+  (:require [clojure.test :refer :all]
+            [shell-do-undo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
